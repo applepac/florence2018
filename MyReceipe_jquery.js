ac25592687af801c7fa2e47003fcccfce8517a1b
applepac <39905826+applepac@users.noreply.github.com>
2018-06-11 02:06:42 -0500

    Add files via upload

---
+++ MyReceipe_jquery.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+  
+// $('.welcome').mouseenter(function({
+// $('.welcome').animate({
+//   opacity:"0.5";
+//   background-color:"green"
+// })
+  
+$('.welcome').click(function(){
+  $('.welcome').animate({
+    height: "500px",
+    width: "500px"
+  }, 2000);
+  
+});
+  
+
+
+$('img').click(function(){
+    $(this).hide()
+  });
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  });
+/* when html document is ready do something */
+
+
