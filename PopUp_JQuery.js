6b57e8ed8870ad5fdd5ca46ca29bb44a0654d57d
applepac <39905826+applepac@users.noreply.github.com>
2018-07-23 02:22:16 -0500

    Add files via upload

---
+++ PopUp_JQuery.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+  
+  console.log("Test");
+  
+// $('#close_button').click(function(){
+//   $('body').animate({display: 'none', opacity: '0'}, 500);
+// });
+
+// $('#close_button').click(function(){
+//   window.parent.close()
+
+
+});
